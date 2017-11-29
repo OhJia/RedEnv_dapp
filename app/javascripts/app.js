095ba7ff1a3218924357fa8960c99f8e2f9ea66f
OhJia <jiajia0919@gmail.com>
2017-11-28 22:26:21 -0500

    clean up html/css/js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -5,33 +5,23 @@ import { default as contract } from 'truffle-contract'
 
 // Import contract artifacts and turn them into usable abstractions.
 import redenvelope_artifacts from '../../build/contracts/RedEnvelope.json'
-
+//
 import { default as moment } from 'moment';
 import { default as BigNumber } from 'bignumber.js';
 
 var RedEnvelope = contract(redenvelope_artifacts);
-
-window.copyToClipboard = window.copyToClipboard || function(element) {
-  console.log("pressed!", $(element).val());
-  console.log(element);
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).val()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
  
 window.App = {
-  start: function() {
-    var self = this;
 
+  start: () => {
+    
     RedEnvelope.setProvider(web3.currentProvider);
     console.log("App started and web3 provider set.")
 
     /* 
     * On create page
     */ 
-    $("#create-envelope").submit(function(event) {
+    $("#create-envelope").submit((event) => {
        const req = {
         amount: $('#amount').val(),
         passcode: $('#passcode').val(),
@@ -48,23 +38,26 @@ window.App = {
     let claimEnvIndex;
 
     if ($("#claim-page").length > 0) {
-      let envIndex = new URLSearchParams(window.location.search).get('env-id');
+      const envIndex = new URLSearchParams(window.location.search).get('env-id');
       console.log("On claim page");
       $("#passcode-not-match").hide();
 
       // If this is a query
       if (envIndex) { 
+        $("#claim-default").hide();
+        $("#unlock-envelope").show();
         claimEnvIndex = envIndex;
-        checkIndex(claimEnvIndex);
+        App.checkIndex(claimEnvIndex);
       } else {
-        window.location.replace("/index.html");
+        $("#unlock-envelope").hide();
+        $("#claim-default").show();
       }
     }
 
     /* 
     * Submit passcode to claim envelope
     */ 
-    $("#claim-enter-passcode").submit(function(event) {
+    $("#claim-enter-passcode").submit((event) => {
        const req = {
          claimPasscode: App.getPasscode(),
          claimIndex: claimEnvIndex
@@ -77,72 +70,88 @@ window.App = {
     });
   },
 
-  getPasscode: function() {
-    return $('#claim-passcode').val();
+  getPasscode: () => { 
+    return $('#claim-passcode').val(); 
   },
 
-  // Create envelope
-  buyEnvelope: function(params) {
-    var self = this;
+  /* 
+  * Create envelope
+  */   
+  buyEnvelope: (params) => {
+    const amountToBuy = params.amount;
+    const amountInWei = web3.toWei(amountToBuy, 'gwei');   
+    const passcode = params.passcode.toString();
 
-    let amountToBuy = params.amount;
-    let amountInWei = web3.toWei(amountToBuy, 'gwei');   
-    let passcode = params.passcode.toString();
-
-    console.log("params:", params);
-
-    //$("#buy-msg").html("Making your envelope. Please wait.");
-
-    RedEnvelope.deployed().then(function(i) {
+    RedEnvelope.deployed().then((i) => {
       i.buyEnvelope(passcode, {from: web3.eth.accounts[0], value: amountInWei}).then(function(f) {
-        //$("#buy-msg").html("");
         $("#container-create").hide();
         renderEnvelopeLink();
-      }).catch(function(e) {
+      }).catch((e) => {
         console.log(e);
-        self.setStatus("Error sending coin; see log.");
+        this.setStatus("Error buying envelope; see log.");
       });
     });
   },
 
 
-  // Claim
+  
+  /* 
+  * Check that envelope exists,
+  * check passcode, 
+  * and claim envelope
+  */ 
+  checkIndex: (id) => {
+    RedEnvelope.deployed().then((i) => {
+      i.envelopeIndex.call().then((index) => {
+        console.log("total envelope index: ", parseInt(index));
+        if (id > parseInt(index)) {
+          console.log("envelope not found");
+          $("#unlock-envelope").hide();
+          $("#envelope-not-found").show();
+          $("#envelope-not-found").html(buildErrorMessage());
+        } else {
+          console.log("found");
+          $("#envelope-not-found").hide();
+          $("#claim-env-index").html(id);
+          $("#unlock-envelope").show();
+        }
+      }).catch((e) => {
+        console.log(e);
+        this.setStatus("Error getting envelope index; see log.");
+      });
+    });
+  },
 
-  checkPasscode: function(params) {
-    var self = this;
+  checkPasscode: (params) => {
+    const passcode = params.claimPasscode;
+    const index = params.claimIndex;
 
-    let passcode = params.claimPasscode;
-    let index = params.claimIndex;
-    console.log(passcode);
-    console.log(index);
-
-    RedEnvelope.deployed().then(function(i) {
-      i.checkPasscode.call(passcode, index, {from: web3.eth.accounts[0]}).then(function(matched) {
+    RedEnvelope.deployed().then((i) => {
+      i.checkPasscode.call(passcode, index, {from: web3.eth.accounts[0]}).then((matched) => {
         console.log("passcode matched: ", matched);
         if (matched) {
           $("#unlock-envelope").hide();
           $("#passcode-not-match").html("");
           renderEnvelopeToClaim(index);
-          // renderClaimButton(index);
         } else {
           console.log("Passcode doesn't match. Try again.");
           $("#passcode-not-match").show();
-          $("#passcode-not-match").html("Passcode doesn't match. Try again.");
+          $("#passcode-not-match").html("<p>Passcode doesn't match. Try again.</p>");
         }
-      }).catch(function(e) {
+      }).catch((e) => {
         console.log(e);
-        self.setStatus("Error sending coin; see log.");
+        this.setStatus("Error checking passcode; see log.");
       });
     });
   },
 
-  claim: function(index) {
-    var self = this;
+  claim: (index) => {
     let claimIndex = index;
-    RedEnvelope.deployed().then(function(i) {
+    RedEnvelope.deployed().then((i) => {
       return i.claim(App.getPasscode(), claimIndex, {from: web3.eth.accounts[0]})
-    }).then(function(result) { 
-          
+    }).then((result) => { 
+      
+      // Look for Claimed event in logs  
       for (var i = 0; i < result.logs.length; i++) {
         var log = result.logs[i];
         if (log.event == "Claimed") {
@@ -150,12 +159,13 @@ window.App = {
           $("#envelope-to-claim").hide();
           $("#claimed-envelope").hide();
           renderClaimedEnvelope(parseInt(log.args._id));
-          $("#claim-info").html(buildClaimInfo(parseInt(log.args._value)));
+          $("#claim-info").show();
+          $("#claim-info").html(buildClaimInfo(parseInt(log.args._value), web3.eth.accounts[0]));
         }
       }        
-    }).catch(function(e) {
+    }).catch((e) => {
       console.log(e);
-      self.setStatus("Error sending coin; see log.");
+      this.setStatus("Error claiming envelope; see log.");
     });
   }
 
@@ -163,13 +173,12 @@ window.App = {
 
 /***************************************************
   RENDER ENVELOPE INFO AFTER CREATE
-
 ****************************************************/
 function renderEnvelopeLink() {  
-  RedEnvelope.deployed().then(function(i) {
-    i.envelopeIndex.call().then(function(index) {
+  RedEnvelope.deployed().then((i) => {
+    i.envelopeIndex.call().then((index) => {
       console.log("index is at: ", index);
-      const link = "/claim.html?env-id=" + index;
+      const link = `/claim.html?env-id=${index}`;
       $("#envelope-link").append(buildEnvelopeLink(link));
       renderEnvelopeCreated(index);
     });
@@ -183,7 +192,7 @@ function buildEnvelopeLink(link) {
   linkField.addClass("row");
   let node1 = $("<div/>");
   node1.addClass("col-sm-9 container-input left-align");
-  node1.append("<input type='text' id='envelope-link-field' value=" + link + ">");
+  node1.append(`<input type='text' id='envelope-link-field' value=${link}>`);
   let node2 = $("<div/>");
   node2.addClass("col-sm-3 container-button");
   node2.append(`<button id='copyBtn' class='btn btn-env' onclick='copyToClipboard("#envelope-link-field")'>COPY</button>`);
@@ -201,9 +210,10 @@ function buildEnvelopeLink(link) {
 }
 
 function renderEnvelopeCreated(index) {
-  RedEnvelope.deployed().then(function(i) {
-    i.getEnvelopeInfo.call(index).then(function(envelope) {
-      $("#envelope-info").append(buildEnvelope(envelope, "create"));
+  RedEnvelope.deployed().then((i) => {
+    i.getEnvelopeInfo.call(index).then((envelope) => {
+      $("#envelope-info").html("");
+      $("#envelope-info").html(buildEnvelope(envelope, 'create'));
     });
   })
 }
@@ -211,70 +221,46 @@ function renderEnvelopeCreated(index) {
 
 /***************************************************
   RENDER ENVELOPE TO CLAIM
-
 ****************************************************/
-
-function checkIndex(id) {
-  console.log("submitted index: ", id);
-  RedEnvelope.deployed().then(function(i) {
-    i.envelopeIndex.call().then(function(index) {
-      console.log("total envelope index: ", parseInt(index));
-      if (id > parseInt(index)) {
-        console.log("envelope not found");
-        $("#unlock-envelope").hide();
-        $("#envelope-not-found").show();
-        $("#envelope-not-found").html("This envelope is not found. Try a different link.");
-      } else {
-        console.log("found");
-        $("#envelope-not-found").hide();
-        $("#claim-env-index").html(id);
-        $("#unlock-envelope").show();
-      }
-    });
-  })
-}
-
 function renderEnvelopeToClaim(index) {
-  console.log("rendering claim envelope #: ", index);
-  RedEnvelope.deployed().then(function(i) {
-    i.getEnvelopeInfo.call(index).then(function(envelope) {
-      $("#envelope-to-claim").html(buildEnvelope(envelope, "claim"));
+  RedEnvelope.deployed().then((i) => {
+    i.getEnvelopeInfo.call(index).then((envelope) => {
+      $("#envelope-to-claim").html(buildEnvelope(envelope, 'claim'));
       $("#envelope-to-claim").show();
     });
   })
 }
 
+function buildErrorMessage() {
+  let node = $("<div/>");
+  node.addClass("container-title");
+  node.append(`<h2>Uh oh. This envelope does not exist.</h2>`);
+  node.append(`<p>Check your URL and paste it into the search bar.</p>`);
+  node.append(`<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1476 399"><defs><style>.cls-1{fill:#d8d8d8;}.cls-2{fill:#e1e7ec;}.cls-3{fill:#eb423e;}.cls-4{fill:#fbb03b;}.cls-5{fill:#7ac943;}.cls-6{fill:#fff;}.cls-7{fill:none;stroke:#eb423e;stroke-miterlimit:10;stroke-width:5px;}</style></defs><title>sb</title><rect class="cls-1" x="18" y="15" width="1438" height="159" rx="12" ry="12"/><rect class="cls-2" x="18" y="105" width="1437" height="169"/><circle class="cls-3" cx="66" cy="61" r="18"/><circle class="cls-4" cx="116" cy="61" r="18"/><circle class="cls-5" cx="166" cy="61" r="18"/><polygon class="cls-2" points="208 115 252 41 456 41 497 112 208 115"/><rect class="cls-6" x="192" y="129" width="1138" height="90" rx="12" ry="12"/><path class="cls-7" d="M715.5,173.75c0,114.74-93,207.75-207.75,207.75"/><polygon class="cls-3" points="715.33 158 695.7 192 734.96 192 715.33 158"/></svg>`);
+  return node;
+}
+
 
 /***************************************************
   RENDER ENVELOPE INFO AFTER CLAIM
-
 ****************************************************/
-// function renderClaimInfo(index, address) {
-//   console.log("rendering claim info for #: ", index);
-//   $("#claim-info").show();
-//   RedEnvelope.deployed().then(function(i) {
-//     i.getClaimInfo.call(index, address).then(function(claim) {
-//       $("#claim-info").html(buildClaimInfo(claim));
-//     });
-//   })
-// }
-
-function buildClaimInfo(claim) {
+function buildClaimInfo(claim, claimer) {
   console.log(claim);
   let claimAmountEth = new BigNumber(web3.fromWei(claim, 'ether')).toFormat(8);
   let claimAmountGwei = new BigNumber(web3.fromWei(claim, 'gwei')).toFormat(2);
   
-  let node = $("<div/>");
+  let node = $(`<div/>`);
   node.append(`<div><h2>You claimed ${claimAmountEth} ETH</h2></div>`);
-  node.append(`<div><p>${claimAmountGwei} GWEI</p></div>`);
+  node.append(`<div><p>(${claimAmountGwei} GWEI)</p></div>`);
+  node.append(`<div><p><strong>Paid to: </strong>${claimer}</p></div>`);
   return node;
 }
 
 function renderClaimedEnvelope(index) {
-  console.log("rendering claimed envelope #: ", index);
-  RedEnvelope.deployed().then(function(i) {
-    i.getEnvelopeInfo.call(index).then(function(envelope) {
-      $("#claimed-envelope").html(buildEnvelope(envelope, "claimed"));
+  RedEnvelope.deployed().then((i) => {
+    i.getEnvelopeInfo.call(index).then((envelope) => {
+      $("#claimed-envelope").html("");
+      $("#claimed-envelope").html(buildEnvelope(envelope, 'claimed'));
       $("#claimed-envelope").show();
     });
   })
@@ -283,9 +269,7 @@ function renderClaimedEnvelope(index) {
 
 /***************************************************
   BUILD ENVELOPE COMPONENTS
-
 ****************************************************/
-
 function buildEnvelope(env, step) {
   console.log(env);
   const [id, creatorAddress, startTime, initialBalance, remainingBalance, totalClaims] = env;
@@ -299,11 +283,11 @@ function buildEnvelope(env, step) {
   let initialBalanceEth = null;
   let initialBalanceGwei = null;
 
-  if (step == "create") { titleText = `<h2>Red Env #${id} created</h2>`; } 
-  else if (step == "claim") { 
+  if (step === 'create') { titleText = `<h2>Red Env #${id} created</h2>`; } 
+  else if (step === 'claim') { 
     titleText = `<h2>Claim Red Env #${id}</h2>`; 
     claimButton = `<div id='claim-button'><button id='claim' class='btn btn-env' onclick='App.claim(${id})'>Claim</button></div>`
-  } else if (step == "claimed") { 
+  } else if (step === 'claimed') { 
     titleText = `<h2>from Red Env #${id}</h2>`; 
     initialBalanceEth = new BigNumber(web3.fromWei(initialBalance, 'ether')).toFormat(4);
     initialBalanceGwei = new BigNumber(web3.fromWei(initialBalance, 'gwei')).toFormat(0);
@@ -319,7 +303,6 @@ function buildEnvelope(env, step) {
 
   let envInfo = $("<div/>");
   envInfo.addClass("envelope-info-area");
-  // envInfo.append("<p><strong>Remaining amount:</strong></p>");
   envInfo.append(claimButton);
   envInfo.append(`<h2>${remainingBalanceEth} ETH</h2>`);
   envInfo.append(`<p><strong>${remainingBalanceGwei} GWEI</strong></p>`);
@@ -327,7 +310,7 @@ function buildEnvelope(env, step) {
   let envDetails = $("<div/>");
   envDetails.addClass("envelope-detail-area left-align");
   envDetails.append(`<p><strong>From: </strong>${creatorAddress}</p>`);
-  envDetails.append(`<p><strong>Created at: </strong>${moment(startTime).format('MM-DD-YYYY HH:MM:SS A')}</p>`);
+  envDetails.append(`<p><strong>Created at: </strong>${moment(startTime * 1000).local().format("YYYY-MM-DD HH:mm:ss")}</p>`);
   envDetails.append(initialBalanceText);
   envDetails.append(totalClaimsText);
 
@@ -337,11 +320,22 @@ function buildEnvelope(env, step) {
   return node;
 }
 
+/***************************************************
+  HELPER
+****************************************************/
+window.copyToClipboard = (element) => {
+  console.log("pressed!", $(element).val());
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).val()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
 
 /***************************************************
   ON LOAD
 ****************************************************/
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
   //Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
